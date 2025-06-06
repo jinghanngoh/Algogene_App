a10e7b846ad71bfce672c69f991f2f2bf8d9896b
jinghanngoh <jinghanngoh@gmail.com>
2025-06-06 13:27:19 +0800

    Empty Auth files
    -

---
+++ services/auth/auth.js
@@ -0,0 +1,11 @@
+// import { googleSignIn } from './googleAuth';
+// import { githubSignIn } from './githubAuth';
+// import { linkedinSignIn } from './linkedinAuth';
+
+// export const AuthService = {
+//   google: googleSignIn,
+//   github: githubSignIn,
+//   linkedin: linkedinSignIn,
+  
+//   // Add other auth methods here
+// };
