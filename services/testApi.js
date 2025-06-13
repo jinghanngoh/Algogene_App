34f975356254684e61ae305ef34afdc6c4194112
jinghanngoh <jinghanngoh@gmail.com>
2025-06-13 15:39:43 +0800

    2.1's API Tidy
    -

--- services/testApi.js
+++ services/testApi.js
@@ -1,84 +1,3 @@
-// // Test out the public marketplace endpoint 
-// import API from './api';
-// import { v4 as uuidv4 } from 'uuid';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// // Generate a 32-character session ID
-// const generateSessionId = () => {
-//   return uuidv4().replace(/-/g, ''); // 32 chars
-// };
-
-// export const fetchPublicAlgos = async () => { 
-//   try {
-//     console.log('Making API Request to /rest/v1/app_mp_topalgo');
-    
-//     // Get or generate session ID
-//     let sessionId = await AsyncStorage.getItem('sessionId');
-//     if (!sessionId) {
-//       sessionId = generateSessionId();
-//       await AsyncStorage.setItem('sessionId', sessionId);
-//     }
-    
-//     const response = await API.get('/rest/v1/app_mp_topalgo', {
-//       params: {
-//         user: 'AGBOT1',
-//         api_key: '13c80d4bd1094d07ceb974baa684cf8ccdd18f4aea56a7c46cc91abf0cc883ff',
-//         sid: sessionId
-//       }
-//     });
-    
-//     console.log('API Response:', response);
-
-//     if (!response.status) {
-//       throw new Error(response.res || 'API returned false status');
-//     }
-
-//     return response.res;
-
-//   } catch (error) {
-//     if (error.response) {
-//       console.error('Error Response Data:', error.response.data);
-//       console.error('Error Response Status:', error.response.status);
-//     } else if (error.request) {
-//       console.error('Error Request:', error.request);
-//     } else {
-//       console.error('Error Message:', error.message);
-//     }
-//     throw error;
-//   }
-// };
-
-
-// // Fetch trading bot performance statistics (new function)
-// // export const fetchAlgoPerformance = async (algoId, accountingDate = null) => {
-// //   try {
-// //     console.log('Making API Request to /rest/v1/strategy_stats');
-// //     let sessionId = await AsyncStorage.getItem('sessionId');
-// //     if (!sessionId) {
-// //       sessionId = generateSessionId();
-// //       await AsyncStorage.setItem('sessionId', sessionId);
-// //     }
-
-// //     const response = await API.get('/rest/v1/strategy_stats', {
-// //       params: {
-// //         user: 'AGBOT1',
-// //         api_key: '13c80d4bd1094d07ceb974baa684cf8ccdd18f4aea56a7c46cc91abf0cc883ff',
-// //         sid: sessionId,
-// //         algo_id: algoId,
-// //         acdate: accountingDate
-// //       }
-// //     });
-
-// //     if (!response.performance) {
-// //       throw new Error('Performance data not available.');
-// //     }
-// //     return response.performance;
-// //   } catch (error) {
-// //     console.error('Error fetching algorithm performance:', error);
-// //     throw error;
-// //   }
-// // };
-
 // Test out the public marketplace endpoint 
 import API from './api';
 import { v4 as uuidv4 } from 'uuid';
@@ -89,6 +8,7 @@ const generateSessionId = () => {
   return uuidv4().replace(/-/g, ''); // 32 chars
 };
 
+// Fetch Trading Bots (2.1)
 export const fetchPublicAlgos = async () => { 
   try {
     console.log('Making API Request to /rest/v1/app_mp_topalgo');
@@ -128,6 +48,36 @@ export const fetchPublicAlgos = async () => {
     } else {
       console.error('Error Message:', error.message);
     }
+    throw error;
+  }
+};
+
+// Fetch trading bot performance statistics (2.2)
+export const fetchAlgoPerformance = async (algoId, accountingDate = null) => {
+  try {
+    console.log('Making API Request to /rest/v1/strategy_stats');
+    let sessionId = await AsyncStorage.getItem('sessionId');
+    if (!sessionId) {
+      sessionId = generateSessionId();
+      await AsyncStorage.setItem('sessionId', sessionId);
+    }
+
+    const response = await API.get('/rest/v1/strategy_stats', {
+      params: {
+        user: 'AGBOT1',
+        api_key: '13c80d4bd1094d07ceb974baa684cf8ccdd18f4aea56a7c46cc91abf0cc883ff',
+        sid: sessionId,
+        algo_id: algoId,
+        acdate: accountingDate
+      }
+    });
+
+    if (!response.performance) {
+      throw new Error('Performance data not available.');
+    }
+    return response.performance;
+  } catch (error) {
+    console.error('Error fetching algorithm performance:', error);
     throw error;
   }
 };
